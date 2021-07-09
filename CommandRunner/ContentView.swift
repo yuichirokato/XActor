22d62761eec4b45e26f40df912fa8d327a759e42
Yuichiro Takahashi <ikmy0709@gmail.com>
2021-07-09 18:43:38 +0900

    Initial Commit

---
+++ CommandRunner/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  CommandRunner
+//
+//  Created by Yuichirou Takahashi on 2021/07/09.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
